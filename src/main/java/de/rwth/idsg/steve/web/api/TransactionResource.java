b3d4d5e51e66694eb5b6b6dcf23f33194b859af5
gilinykh <gilinykh@gramant.ru>
2019-04-23 15:33:29 +0300

    Move transactions query API under tag resource

--- src/main/java/de/rwth/idsg/steve/web/api/TransactionResource.java
+++ src/main/java/de/rwth/idsg/steve/web/api/TransactionResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/transactions")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class TransactionResource {
 
@@ -19,24 +19,24 @@ public class TransactionResource {
 
     private TransactionRepository transactionRepository;
 
-    @PostMapping
+    @PostMapping("/transactions")
     public ResponseEntity<Void> start(@RequestBody TransactionStartRequest request) {
         client16.remoteStartTransaction(request.asParams());
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/{idTag}")
+    @GetMapping("/tags/{idTag}/transactions")
     public ResponseEntity<List<Transaction>> userTransactions(@PathVariable String idTag) {
         TransactionListRequest request = new TransactionListRequest(idTag);
         return ResponseEntity.ok(transactionRepository.getTransactions(request.asQuery()));
     }
 
-    @GetMapping("/{transactionId}")
+    @GetMapping("/transactions/{transactionId}")
     public ResponseEntity<TransactionDetails> transactionDetails(@PathVariable Integer transactionId) {
         return ResponseEntity.ok(transactionRepository.getDetails(transactionId));
     }
 
-    @DeleteMapping
+    @DeleteMapping("/transactions")
     public ResponseEntity<Void> stop(@RequestBody TransactionStopRequest request) {
         client16.remoteStopTransaction(request.asParams());
         return ResponseEntity.noContent().build();
