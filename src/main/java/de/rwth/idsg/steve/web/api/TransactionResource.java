744c5014f620f2ef7c9da3160894ba8cff90c211
gilinykh <gilinykh@gramant.ru>
2019-04-24 15:05:15 +0300

    Implement tx polling after remote start to return txId

--- src/main/java/de/rwth/idsg/steve/web/api/TransactionResource.java
+++ src/main/java/de/rwth/idsg/steve/web/api/TransactionResource.java
@@ -4,11 +4,16 @@ import de.rwth.idsg.steve.repository.TransactionRepository;
 import de.rwth.idsg.steve.repository.dto.Transaction;
 import de.rwth.idsg.steve.repository.dto.TransactionDetails;
 import de.rwth.idsg.steve.service.ChargePointService16_Client;
+import de.rwth.idsg.steve.web.dto.ocpp.RemoteStartTransactionParams;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/api")
@@ -19,10 +24,18 @@ public class TransactionResource {
 
     private TransactionRepository transactionRepository;
 
+    private final TransactionService transactionService;
+
     @PostMapping("/transactions")
     public ResponseEntity<Void> start(@RequestBody TransactionStartRequest request) {
         client16.remoteStartTransaction(request.asParams());
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/transactions/active")
+    public ResponseEntity<Integer> started(@RequestBody TransactionStartRequest request) {
+        Integer transactionId = transactionService.startedTransactionId(request.asParams());
+        return ResponseEntity.ok(transactionId);
     }
 
     @GetMapping("/tags/{idTag}/transactions")
@@ -40,5 +53,38 @@ public class TransactionResource {
     public ResponseEntity<Void> stop(@RequestBody TransactionStopRequest request) {
         client16.remoteStopTransaction(request.asParams());
         return ResponseEntity.noContent().build();
+    }
+
+    @Service
+    @AllArgsConstructor
+    static class TransactionService {
+        private final ChargePointService16_Client client16;
+        private final TransactionRepository transactionRepository;
+
+        public int startedTransactionId(RemoteStartTransactionParams params) {
+            client16.remoteStartTransaction(params);
+
+            String chargeBoxId = params.getChargePointSelectList().get(0).getChargeBoxId();
+            int txPollingTimeout = 5;
+            return getFirstActiveTransactionId(chargeBoxId, txPollingTimeout)
+                    .orElseThrow(() -> new RuntimeException("Transaction hasn't been started within [" + txPollingTimeout + "] seconds interval with Charging Point [" + chargeBoxId + "]"));
+        }
+
+        private Optional<Integer> getFirstActiveTransactionId(String chargeBoxId, int timeoutSec) {
+            Supplier<Long> nowMillis = () -> Instant.now().toEpochMilli();
+            long startMillis = nowMillis.get();
+            long currentMillis = startMillis;
+            List<Integer> transactionIds;
+
+            while(currentMillis < startMillis + timeoutSec * 1000) {
+                transactionIds = transactionRepository.getActiveTransactionIds(chargeBoxId);
+                if (transactionIds.size() > 0) {
+                    return Optional.of(transactionIds.get(0));
+                }
+                currentMillis = nowMillis.get();
+            }
+
+            return Optional.empty();
+        }
     }
 }
