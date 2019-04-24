bb7bb68da5e8dfe4a8ec681ea6e9e422cf5bdd52
gilinykh <gilinykh@gramant.ru>
2019-04-24 18:59:38 +0300

    Draft charging session API

--- src/main/java/de/rwth/idsg/steve/web/api/ChargePointResource.java
+++ src/main/java/de/rwth/idsg/steve/web/api/ChargePointResource.java
@@ -2,12 +2,11 @@ package de.rwth.idsg.steve.web.api;
 
 import de.rwth.idsg.steve.repository.ChargePointRepository;
 import de.rwth.idsg.steve.repository.dto.ChargePoint;
+import de.rwth.idsg.steve.repository.dto.TransactionDetails;
 import de.rwth.idsg.steve.web.dto.ChargePointQueryForm;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,23 @@ public class ChargePointResource {
     public ResponseEntity<List<ChargePoint.Overview>> list() {
         List<ChargePoint.Overview> result = chargePointRepository.getOverview(new ChargePointQueryForm());
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/{chargeBoxId}/session/{idTag}")
+    public ResponseEntity<Void> startSession(@PathVariable String chargeBoxId, @PathVariable String idTag) {
+        // TODO: start session
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{chargeBoxId}/session/{idTag}")
+    public ResponseEntity<TransactionDetails> getSession(@PathVariable String chargeBoxId, @PathVariable String idTag) {
+        // TODO: provide most recently finished transaction details
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{chargeBoxId}/session/{idTag}")
+    public ResponseEntity<Void> endSession(@PathVariable String chargeBoxId, @PathVariable String idTag) {
+        // TODO: find and stop most recently started transaction
+        return ResponseEntity.noContent().build();
     }
 }
